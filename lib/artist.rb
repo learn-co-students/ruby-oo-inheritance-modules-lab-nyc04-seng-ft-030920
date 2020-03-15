48791572a7bc88fe2323017366f1fb72e05d4bd3
replacem3nts <donny.landis@gmail.com>
2020-03-14 20:13:06 -0400

    performs DRY from codealong

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,30 +1,18 @@
-require 'pry'
-
 class Artist
   attr_accessor :name
   attr_reader :songs
+  include Memorable::InstanceMethods, Paramable 
+  extend Memorable::ClassMethods, Findable
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
@@ -34,9 +22,5 @@ class Artist
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
   end
 end
